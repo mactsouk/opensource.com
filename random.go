7e11c5211f2a8aa1d6d3c362e5beb836d8f7e21a
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-05-01 17:49:58 +0300

    Update random.go

--- random.go
+++ random.go
@@ -1,1 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func main() {
+	MIN := 0
+	MAX := 0
+	TOTAL := 0
+	if len(os.Args) > 3 {
+		MIN, _ = strconv.Atoi(os.Args[1])
+		MAX, _ = strconv.Atoi(os.Args[2])
+		TOTAL, _ = strconv.Atoi(os.Args[3])
+	} else {
+		fmt.Println("Usage:", os.Args[0], "MIX MAX TOTAL")
+		return
+	}
+
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < TOTAL; i++ {
+		myrand := random(MIN, MAX)
+		fmt.Print(myrand)
+		fmt.Print(" ")
+	}
+	fmt.Println()
+}
 
